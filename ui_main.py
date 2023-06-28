97129179f383c11af4ea6c54ccae49105bdb72af
Emanuel-Tarcisiu <orsanemanuel@yahoo.com>
2023-06-28 11:31:44 +0300

    miercuri 28.06

--- ui_main.py
+++ ui_main.py
@@ -1,42 +1,52 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, simpledialog
 import subprocess
-
-
 
 def execute_option(option):
     if option == "Backup":
-        result = subprocess.call(["./backups.sh", "/home/emy"])
+        password = get_password()
+        command = f"sudo ./backups.sh /home/emy {password}"
+        result = subprocess.call(command, shell=True)
     elif option == "Sync":
-        result = subprocess.call(["./sync.sh"])
+        password = get_password()
+        command = f"sudo ./sync.sh {password}"
+        result = subprocess.call(command, shell=True)
     elif option == "Cleanup":
-        result = subprocess.call(["./cleanup.sh", "/home/emy"])
+        password = get_password()
+        command = f"sudo ./cleanup.sh /home/emy {password}"
+        result = subprocess.call(command, shell=True)
     elif option == "Updates":
-        result = subprocess.call(["sudo", "./updates.sh"])
+        password = get_password()
+        command = f"sudo ./updates.sh {password}"
+        result = subprocess.call(command, shell=True)
     elif option == "EXIT":
-        print("Programul se va inchide in 3sec")
+        print("Programul se va inchide in 3 secunde")
         window.after(3000, window.destroy)
         return
-
 
     if result == 0:
         messagebox.showinfo("Script Result", f"{option} script finished successfully!")
     else:
         messagebox.showerror("Script Result", f"{option} script encountered an error!")
 
+def get_password():
+    password = simpledialog.askstring("Password", "Enter your password:", show="*")
+    return password
 
 window = tk.Tk()
 window.title("Script Selector by Orsan")
 window.geometry("400x400")
 
-
 options = ["Backup", "Sync", "Cleanup", "Updates", "EXIT"]
 
-
 for option in options:
-    button = tk.Button(window, text=option, width=20, height=3,
-                       command=lambda opt=option: execute_option(opt))
+    button = tk.Button(
+        window,
+        text=option,
+        width=20,
+        height=3,
+        command=lambda opt=option: execute_option(opt),
+    )
     button.pack(pady=5)
-
 
 window.mainloop()
